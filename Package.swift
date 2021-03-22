f465dd5c0bf590cdb9bee4e96c53b1719ee30730
Chris Brind <brindy@duckduckgo.com>
2021-03-22 22:07:19 +0000

    SQLCipher support (GRDB 5.26.0, SQLCipher 4.5.1)

--- Package.swift
+++ Package.swift
@@ -18,13 +18,62 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .systemLibrary(
-            name: "CSQLite",
-            providers: [.apt(["libsqlite3-dev"])]),
         .target(
             name: "GRDB",
-            dependencies: ["CSQLite"],
-            path: "GRDB"),
+            dependencies: ["SQLCipher"],
+            path: "GRDB",
+            cSettings: [
+                 .define("SQLITE_HAS_CODEC"),
+                 .define("SQLITE_TEMP_STORE", to: "2"),
+                 .define("SQLITE_SOUNDEX"),
+                 .define("SQLITE_THREADSAFE"),
+                 .define("SQLITE_ENABLE_RTREE"),
+                 .define("SQLITE_ENABLE_STAT3"),
+                 .define("SQLITE_ENABLE_STAT4"),
+                 .define("SQLITE_ENABLE_COLUMN_METADATA"),
+                 .define("SQLITE_ENABLE_MEMORY_MANAGEMENT"),
+                 .define("SQLITE_ENABLE_LOAD_EXTENSION"),
+                 .define("SQLITE_ENABLE_FTS4"),
+                 .define("SQLITE_ENABLE_FTS4_UNICODE61"),
+                 .define("SQLITE_ENABLE_FTS3_PARENTHESIS"),
+                 .define("SQLITE_ENABLE_UNLOCK_NOTIFY"),
+                 .define("SQLITE_ENABLE_JSON1"),
+                 .define("SQLITE_ENABLE_FTS5"),
+                 .define("SQLCIPHER_CRYPTO_CC"),
+                 .define("HAVE_USLEEP", to: "1"),
+                 .define("SQLITE_MAX_VARIABLE_NUMBER", to: "99999")
+             ],
+             swiftSettings: [
+                 .define("SQLITE_HAS_CODEC"),
+                 .define("GRDBCIPHER"),
+                 .define("SQLITE_ENABLE_FTS5")
+             ]),
+        .target(
+             name: "SQLCipher",
+             cSettings: [
+                 .define("NDEBUG"),
+                 .define("SQLITE_HAS_CODEC"),
+                 .define("SQLITE_TEMP_STORE", to: "2"),
+                 .define("SQLITE_SOUNDEX"),
+                 .define("SQLITE_THREADSAFE"),
+                 .define("SQLITE_ENABLE_RTREE"),
+                 .define("SQLITE_ENABLE_STAT3"),
+                 .define("SQLITE_ENABLE_STAT4"),
+                 .define("SQLITE_ENABLE_COLUMN_METADATA"),
+                 .define("SQLITE_ENABLE_MEMORY_MANAGEMENT"),
+                 .define("SQLITE_ENABLE_LOAD_EXTENSION"),
+                 .define("SQLITE_ENABLE_FTS4"),
+                 .define("SQLITE_ENABLE_FTS4_UNICODE61"),
+                 .define("SQLITE_ENABLE_FTS3_PARENTHESIS"),
+                 .define("SQLITE_ENABLE_UNLOCK_NOTIFY"),
+                 .define("SQLITE_ENABLE_JSON1"),
+                 .define("SQLITE_ENABLE_FTS5"),
+                 .define("SQLCIPHER_CRYPTO_CC"),
+                 .define("HAVE_USLEEP", to: "1"),
+                 .define("SQLITE_MAX_VARIABLE_NUMBER", to: "99999"),
+                 .define("HAVE_GETHOSTUUID", to: "0"),
+                 // .unsafeFlags(["-w"])
+             ]),
         .testTarget(
             name: "GRDBTests",
             dependencies: ["GRDB"],
