4eb9d27fe66166efdcc713952b019c95900562dc
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-01-15 11:37:19 +0100

    Sync with release/1.2.1-grdb-5.26.0-sqlcipher-4.5.1 branch

--- Sources/SQLCipher/include/SQLCipher_config.h
+++ Sources/SQLCipher/include/SQLCipher_config.h
@@ -3,11 +3,11 @@
 
 #include "sqlite3.h"
 
-typedef void(*_errorLogCallback)(void *pArg, int iErrCode, const char *zMsg);
+typedef void(*errorLogCallback)(void *pArg, int iErrCode, const char *zMsg);
 
 /// Wrapper around sqlite3_config(SQLITE_CONFIG_LOG, ...) which is a variadic
 /// function that can't be used from Swift.
-static inline void registerErrorLogCallback(_errorLogCallback callback) {
+static inline void registerErrorLogCallback(errorLogCallback callback) {
     sqlite3_config(SQLITE_CONFIG_LOG, callback, 0);
 }
 
